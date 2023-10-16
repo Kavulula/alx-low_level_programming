3de4b2bcb2c2e5692b9a52592a690f4c70c823ac
Kavulula <ndambukifernando@gmail.com>
2023-10-16 13:33:37 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char C;
+
+	c = 'a';
+	while
+		(C < = 'z') {
+			putchar(C);
+			C++;
+		}
+	putchar('\n');
+	return (0);
